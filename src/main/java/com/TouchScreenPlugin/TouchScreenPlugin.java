d69cf0120ebf88706103af20fec1c62002339ed6
Colton Campbell <ColtonJamesCampbell@gmail.com>
2023-10-25 22:20:15 -0300

    Added ability to scroll certain ui elements by dragging.

--- src/main/java/com/TouchScreenPlugin/TouchScreenPlugin.java
+++ src/main/java/com/TouchScreenPlugin/TouchScreenPlugin.java
@@ -7,23 +7,27 @@ import javax.swing.*;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.MenuEntry;
 import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.input.MouseListener;
 import net.runelite.client.input.MouseManager;
+import net.runelite.client.input.MouseWheelListener;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 @Slf4j
 @PluginDescriptor(
 	name = "Touch Screen"
 )
-public class TouchScreenPlugin extends Plugin implements MouseListener
+public class TouchScreenPlugin extends Plugin implements MouseListener, MouseWheelListener
 {
 	@Inject
 	private Client client;
@@ -41,8 +45,13 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 	private boolean forceLeftClick = false;
 	private boolean isRotating = false;
 	private boolean canRotate = false;
+
+	private boolean isScrolling = false;
+	private Component scrollComponent = null;
+
 	private boolean guiDown = false;
 	private Point dragStartPoint = null;
+	private Point previousMousePointForScroll = null;
 
 	private WidgetInfo[] quickWidgets = new WidgetInfo[] {
 		WidgetInfo.MINIMAP_QUICK_PRAYER_ORB,
@@ -51,9 +60,30 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 		WidgetInfo.MINIMAP_HEALTH_ORB
 	};
 
+	// Widgets that cause scrolling when dragged
+	private WidgetInfo[] scrollWidgets = new WidgetInfo[] {
+
+	};
+
+	// Widgets that cause scrolling when dragged, do not check their children
+	private WidgetInfo[] scrollWidgets_noChild = new WidgetInfo[] {
+			WidgetInfo.CHATBOX,
+			WidgetInfo.BANK_ITEM_CONTAINER,
+			WidgetInfo.QUESTLIST_CONTAINER,
+			WidgetInfo.ACHIEVEMENT_DIARY_CONTAINER,
+			WidgetInfo.WORLD_SWITCHER_LIST,
+
+			//
+			WidgetInfo.FIXED_VIEWPORT_MINIMAP_DRAW_AREA,
+			WidgetInfo.RESIZABLE_MINIMAP_DRAW_AREA
+	};
+
+	// Widgets that suppress camera rotation when dragged
 	private WidgetInfo[] dragWidgets = new WidgetInfo[] {
 			WidgetInfo.BANK_CONTAINER,
 			WidgetInfo.BANK_INVENTORY_ITEMS_CONTAINER,
+			WidgetInfo.RESIZABLE_VIEWPORT_INVENTORY_CONTAINER,
+			WidgetInfo.RESIZABLE_VIEWPORT_BOTTOM_LINE_INVENTORY_CONTAINER,
 			WidgetInfo.WORLD_MAP_VIEW,
 			WidgetInfo.FIXED_VIEWPORT_INVENTORY_CONTAINER,
 			WidgetInfo.RESIZABLE_VIEWPORT_INVENTORY_CONTAINER,
@@ -61,11 +91,8 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 			WidgetInfo.DEPOSIT_BOX_INVENTORY_ITEMS_CONTAINER,
 			WidgetInfo.GENERIC_SCROLL_TEXT,
 			WidgetInfo.WORLD_SWITCHER_LIST,
-			WidgetInfo.WORLD_SWITCHER_BUTTON
-	};
-
-	private WidgetInfo[] dragWidgets_noChild = new WidgetInfo[] {
-			WidgetInfo.CHATBOX_PARENT
+			WidgetInfo.WORLD_SWITCHER_BUTTON,
+			WidgetInfo.BANK_SCROLLBAR
 	};
 
 	@Override
@@ -75,12 +102,14 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 		forceLeftClick = false;
 		isRotating = false;
 		mouseManager.registerMouseListener(this);
+		mouseManager.registerMouseWheelListener(this);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		mouseManager.unregisterMouseListener(this);
+		mouseManager.unregisterMouseWheelListener(this);
 	}
 
 	@Provides
@@ -95,14 +124,31 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 			return mouseEvent;
 		}
 
-		// Avoid adding lag to prayer flicking and such
-		if (forceLeftClick || mouseIsOverGui(mouseEvent.getPoint(), quickWidgets, false)) {
+		if (forceLeftClick) {
 			forceLeftClick = false;
 			return mouseEvent;
 		}
 
+		// Avoid adding lag to prayer flicking, run toggling, etc.
+		if (mouseIsOverGui(mouseEvent.getPoint(), quickWidgets, false)) {
+			return mouseEvent;
+		}
+
 		clientThread.invokeLater(() -> {
-			if (leftMouseButtonDown && isMouseOverDragWidget(mouseEvent.getPoint(), true)) {
+			if (!leftMouseButtonDown) {
+				return;
+			}
+
+			boolean isItemUnderMouse = -1 != findItemIdUnderMouse();
+
+			if (!isItemUnderMouse && isMouseOverScrollWidget(mouseEvent.getPoint(), true)) {
+				isScrolling = true;
+				scrollComponent = mouseEvent.getComponent();
+				canRotate = false;
+				return;
+			}
+
+			if (isItemUnderMouse || isMouseOverDragWidget(mouseEvent.getPoint(), true)) {
 				forceLeftClick = true;
 				guiDown = true;
 				canRotate = false;
@@ -114,7 +160,9 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 
 		guiDown = false;
 		canRotate = true;
+		isScrolling = false;
 		dragStartPoint = mouseEvent.getPoint();
+		previousMousePointForScroll = mouseEvent.getPoint();
 		leftMouseButtonDown = true;
 
 		mouseEvent.consume();
@@ -123,25 +171,73 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 
 	@Override
 	public MouseEvent mouseDragged(MouseEvent mouseEvent) {
-		if (!leftMouseButtonDown || !isLeftMouse(mouseEvent) || !canRotate) {
+		if (!leftMouseButtonDown || !isLeftMouse(mouseEvent)) {
 			return mouseEvent;
 		}
 
-		if (isRotating || getDistance(dragStartPoint, mouseEvent.getPoint()) > config.touchDragThreshold()) {
-			if (!isRotating) {
-				// Fire middle mouse click event to start camera rotation
-				Component component = mouseEvent.getComponent();
-				component.dispatchEvent(
-					rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_PRESSED, MouseEvent.BUTTON2, true)
-				);
-			}
-			isRotating = true;
+		if (isScrolling) {
+			return emulateScrolling(mouseEvent);
+		}
 
-			// Replace the left mouse drag with a middle mouse drag
-			mouseEvent = rebuildMouseEvent(mouseEvent, mouseEvent.getID(), MouseEvent.BUTTON2, false);
+		if (canRotate) {
+			return emulateCameraRotate(mouseEvent);
 		}
 
 		return mouseEvent;
+	}
+
+	private MouseEvent emulateCameraRotate(MouseEvent mouseEvent) {
+		boolean isUnderThreshold = config.touchDragThreshold() > getDistance(dragStartPoint, mouseEvent.getPoint());
+		if (!isRotating && isUnderThreshold) {
+			return mouseEvent;
+		}
+
+		if (!isRotating) {
+			// Fire middle mouse click event to start camera rotation
+			Component component = mouseEvent.getComponent();
+			component.dispatchEvent(
+					rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_PRESSED, MouseEvent.BUTTON2, true)
+			);
+		}
+		isRotating = true;
+
+		// Replace the left mouse drag with a middle mouse drag
+		return rebuildMouseEvent(mouseEvent, mouseEvent.getID(), MouseEvent.BUTTON2, false);
+	}
+
+	private MouseEvent emulateScrolling(MouseEvent mouseEvent) {
+		int deltaY = mouseEvent.getY() - previousMousePointForScroll.y;
+		int effectiveDeltaY = deltaY / -8;
+
+		if (effectiveDeltaY != 0 && scrollComponent != null) {
+			previousMousePointForScroll = mouseEvent.getPoint();
+			scrollComponent.dispatchEvent(new MouseWheelEvent(
+					scrollComponent,
+					MouseWheelEvent.MOUSE_WHEEL,
+					mouseEvent.getWhen(),
+					mouseEvent.getModifiersEx(),
+					dragStartPoint.x,
+					dragStartPoint.y,
+					mouseEvent.getClickCount(),
+					false,
+					MouseWheelEvent.WHEEL_UNIT_SCROLL,
+					1,
+					effectiveDeltaY
+			));
+		}
+
+		// Prevent the mouse from moving from the client's perspective
+		return new MouseEvent(
+				mouseEvent.getComponent(),
+				mouseEvent.getID(),
+				mouseEvent.getWhen(),
+				mouseEvent.getModifiersEx(),
+				dragStartPoint.x,
+				dragStartPoint.y,
+				mouseEvent.getClickCount(),
+				false,
+				mouseEvent.getButton()
+		);
 	}
 
 	@Override
@@ -153,38 +249,50 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 		leftMouseButtonDown = false;
 		Component component = mouseEvent.getComponent();
 
+		if (guiDown) {
+			return mouseEvent;
+		}
+
 		if (isRotating) {
 			isRotating = false;
 			component.dispatchEvent(
 				rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_RELEASED, MouseEvent.BUTTON2, true)
 			);
-		} else {
-			mouseEvent.consume();
-
-			final long t = System.currentTimeMillis();
-			// We need to be on the client thread to properly check widget visibility
-			clientThread.invokeLater(() -> {
-				// Wait a few ms to fix the dead click bug on touch screens
-				// Is RS polling the mouse position somewhere instead of using events?
-				// We also fire immediately if you're over a drag gui
-				long delta = System.currentTimeMillis() - t;
-				if (delta < config.touchDelayMs() && !isMouseOverDragWidget(mouseEvent.getPoint(), true)) {
-					return false;
-				}
-
-				if (!guiDown) {
-					forceLeftClick = true;
-					mouseEvent.getComponent().dispatchEvent(
-							rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_PRESSED, MouseEvent.BUTTON1, true)
-					);
-				}
-
-				mouseEvent.getComponent().dispatchEvent(
-						rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_RELEASED, MouseEvent.BUTTON1, true)
-				);
-				return true;
-			});
+			return mouseEvent;
 		}
+
+		if (isScrolling) {
+			isScrolling = false;
+			mouseEvent.consume();
+			return mouseEvent;
+		}
+
+		// Emulate left click
+
+		mouseEvent.consume();
+
+		final long t = System.currentTimeMillis();
+		// We need to be on the client thread to properly check widget visibility
+		clientThread.invokeLater(() -> {
+			// Wait a few ms to fix the dead click bug on touch screens
+			// Is RS polling the mouse position somewhere instead of using events?
+			long delta = System.currentTimeMillis() - t;
+			if (delta < config.touchDelayMs()) {
+				return false;
+			}
+
+			// Wait, could I just force a MouseMoved event instead of delaying?
+
+			forceLeftClick = true;
+			mouseEvent.getComponent().dispatchEvent(
+					rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_PRESSED, MouseEvent.BUTTON1, true)
+			);
+			mouseEvent.getComponent().dispatchEvent(
+					rebuildMouseEvent(mouseEvent, MouseEvent.MOUSE_RELEASED, MouseEvent.BUTTON1, true)
+			);
+			return true;
+		});
+
 		return mouseEvent;
 	}
 
@@ -193,7 +301,23 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 	@Override
 	public MouseEvent mouseExited(MouseEvent mouseEvent) { return mouseEvent; }
 	@Override
-	public MouseEvent mouseMoved(MouseEvent mouseEvent) { return mouseEvent; }
+	public MouseEvent mouseMoved(MouseEvent mouseEvent) {
+		if (isScrolling) {
+			mouseEvent = new MouseEvent(
+					mouseEvent.getComponent(),
+					mouseEvent.getID(),
+					mouseEvent.getWhen(),
+					mouseEvent.getModifiersEx(),
+					dragStartPoint.x,
+					dragStartPoint.y,
+					mouseEvent.getClickCount(),
+					false,
+					mouseEvent.getButton()
+			);
+		}
+
+		return mouseEvent;
+	}
 	@Override
 	public MouseEvent mouseClicked(MouseEvent mouseEvent) { return mouseEvent; }
 
@@ -202,8 +326,13 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 		return SwingUtilities.isLeftMouseButton(mouseEvent);
 	}
 
+	private boolean isMouseOverScrollWidget(Point point, boolean onThread) {
+		return mouseIsOverGui(point, scrollWidgets, onThread) ||
+				mouseIsOverGui_noChild(point, scrollWidgets_noChild, onThread);
+	}
+
 	private boolean isMouseOverDragWidget(Point point, boolean onThread) {
-		return mouseIsOverGui_noChild(point, dragWidgets_noChild, onThread) || mouseIsOverGui(point, dragWidgets, onThread);
+		return mouseIsOverGui(point, dragWidgets, onThread);
 	}
 
 	private boolean mouseIsOverGui(Point point, WidgetInfo[] widgets, boolean onThread)
@@ -231,13 +360,21 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 
 		return false;
 	}
-
 	private boolean testWidgetRecursive(Widget widget, net.runelite.api.Point point, boolean onThread)
 	{
 		if (testWidget(widget, point, onThread)) {
 			return true;
 		}
 
+		if (testWidgetChildren(widget, point, onThread)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean testWidgetChildren(Widget widget, net.runelite.api.Point point, boolean onThread)
+	{
 		// Got some NPEs from the children accessors in some situations, hence the trys
 		try {
 			Widget[] widgets = widget.getChildren();
@@ -304,5 +441,40 @@ public class TouchScreenPlugin extends Plugin implements MouseListener
 			mouseEvent.isPopupTrigger(),
 			button
 		);
+	}
+
+	private Widget findWidgetUnderMouse() {
+		MenuEntry[] rightClickMenu = client.getMenuEntries();
+		if (rightClickMenu.length == 0) {
+			return null;
+		}
+
+		MenuEntry menuEntry = rightClickMenu[rightClickMenu.length - 1];
+		return menuEntry.getWidget();
+	}
+
+	private int findItemIdUnderMouse() {
+		Widget widget = findWidgetUnderMouse();
+		return widget == null ? -1 : widget.getItemId();
+	}
+
+	@Override
+	public MouseWheelEvent mouseWheelMoved(MouseWheelEvent event) {
+		if (isScrolling) {
+			event = new MouseWheelEvent(
+					event.getComponent(),
+					event.getID(),
+					event.getWhen(),
+					event.getModifiersEx(),
+					dragStartPoint.x,
+					dragStartPoint.y,
+					event.getClickCount(),
+					event.isPopupTrigger(),
+					event.getScrollType(),
+					event.getScrollAmount(),
+					event.getWheelRotation()
+			);
+		}
+		return event;
 	}
 }
